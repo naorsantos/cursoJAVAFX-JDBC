4fbd6da7f57ef458416adddd5444870bec8ae6c9
Naor Santos <naorsantos@gmail.com>
2021-01-27 10:09:29 -0300

    Departamento View

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,36 +1,66 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
-	
+
 	@FXML
 	private MenuItem menuItemVendedor;
-	
+
 	@FXML
 	private MenuItem menuItemDepartamento;
-	
+
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
-	public void onMenuItemSellerAction() {	
+	public void onMenuItemSellerAction() {
 	}
-	
+
 	@FXML
-	public void onMenuItemDepartmentAction() {	
+	public void onMenuItemDepartmentAction() {
+		loadView("/gui/DepartmentList.fxml");
 	}
-	
+
 	@FXML
-	public void onMenuItemAboutAction() {	
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml");
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+	}
+
+	private synchronized void loadView(String absolutName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro carregando a pagina", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
