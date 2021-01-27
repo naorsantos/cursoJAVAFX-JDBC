c235c66f53e616becaa7702e920e33bae65d1766
Naor Santos <naorsantos@gmail.com>
2021-01-27 10:41:33 -0300

    Classe DepartmentService

---
+++ src/gui/DepartmentListController.java
@@ -0,0 +1,70 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
+import model.services.DepartmentServices;
+
+public class DepartmentListController implements Initializable {
+	
+	private DepartmentServices service;
+
+	@FXML
+	private TableView<Department> tableViewDepartments;
+
+	@FXML
+	private TableColumn<Department, Integer> tableColumnId;
+
+	@FXML
+	private TableColumn<Department, String> tableColumnName;
+
+	@FXML
+	private Button btNew;
+	
+	private ObservableList<Department> obsList;
+
+	@FXML
+	public void onbtNewAction() {
+		System.out.println("onbtAction");
+	}
+	
+	public void setDepartmentServices(DepartmentServices service) {
+		this.service = service;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNode();
+
+	}
+
+	private void initializeNode() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartments.prefHeightProperty().bind(stage.heightProperty());
+
+	}
+	
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartments.setItems(obsList);
+	}
+}
